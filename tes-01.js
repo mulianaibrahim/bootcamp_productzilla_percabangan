0349d908b53dbda4de5578c60fa657b7c3125a28
FIRST_NAME LAST_NAME <mulianaibrahim@gmail.com>
2023-03-06 21:46:20 +0700

    latihanpercabangan

---
+++ tes-01.js
@@ -0,0 +1,23 @@
+/*
+Sumbul ingin memesan sebuah sepatu. 
+Akan tetapi dia ingin membeli sepatu ketika cuaca sedang baik. 
+Jadi kalau hujan dia akan beli dari online. kemudian, dia ingin sepatu warnah hitam, akan tetapi di toko hanya ada warna putih. 
+jadi dia akan membeli juga kaus kaki warna putih
+*/
+let cuaca = 'cerah';
+let adaSepatuHitam = true;
+
+if (cuaca == 'cerah') {
+    console.log('ke toko sepatu');
+    console.log('beli sepatu putih');
+    console.log('tidak ada sepatu hitam');
+} else {
+    console.log('pesan shope');
+    if (!adaSepatuHitam) {
+        console.log('tidak ada sepatu hitam');
+    } else {
+        console.log('ada sepatu hitam');
+        console.log('beli kaus kaki putih');
+    }
+    console.log('checkout sepatu');
+}
